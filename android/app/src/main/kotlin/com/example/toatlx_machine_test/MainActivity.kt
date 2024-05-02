26ee70d18e20565b0a679b4cd96d2622b73e6076
ebinanony <ebinantony0088@gmail.com>
2024-05-02 20:44:43 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/toatlx_machine_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.toatlx_machine_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/view/splashscreen/splash_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:toatlx_machine_test/view/homescreen/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (_) => HomeScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // You can replace this with your TotalX logo
+            Image.asset(
+              'assets/totalx_logo.png',
+              width: 150,
+              height: 150,
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(
+              backgroundColor: Colors.white,
+              strokeWidth: 2,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
